f28df7547a6ad1630c095df5da01ff487b93688a
Landon <74lcheung@gmail.com>
2023-12-06 21:17:32 -0500

    update file path for safety, add comments (still need to finish spiting file name from path)

--- AUDswap_backend_SAFE.py
+++ AUDswap_backend_SAFE.py
@@ -2,57 +2,66 @@ from timeit import default_timer as timer
 from pydub import AudioSegment
 
 
-#start = timer()
 
-# files
-# convert to file selection 'browse as input
-src = "transcript.mp3"
-dst = "test.wav"
+def convert(src, dst, to_format):
 
 
-
-
-# sorce <src> is converted to wav (live audio may need to be convertes every 16 bits/sample/'data chunk')
-def convert(src):
+	#list of allowed extensions
 	supported_extensions = "wav, mp4, mp3, ogg, pcm"
-	#set converted file to wav
-	to_format='wav'
-	from_format= "mp3"
 
 	#exit if there is more than one or no extension
-	if len(src) !=2:
+	if len(src.spit(".")) !=2:
 		return "Error, file name must only have one extension"
+
 
 	# get filename and extension
 	filename, from_format=src.split('.')
 
-	#check if file type is convertable 
-	#needs extra function to return is all are 'err'
+
+
+	#check if file type is convertable
+	
+	
 	n=supported_extensions.split(', ')
 	i=1
+
+	#check if all extensions are supported (compares through extension list)
 	for str in n:
+
+		#if from_format is not the same as selected extension
 		if from_format != str:
 
-			if i == len(n):
-				return "Error, file extension unsupported"
-			
+
+			#increment i (counter)
 			i+=1
+
+			#check if all extensions have been searched through w/ counter
+			if i == len(n):
+
+
+				#error mesage if file is unsupported and terminates function
+				return "Error, file extension unsupported"
+
+			#run loop again if the counter has not reached the end of the supported extensions
 			pass
 
+
 		if from_format == str:
-			#print("supported")
+			#if file extension is a match, exit loop and continue the program
 			continue
 
-	#exit if input is already WAV
-	if from_format == 'wav':
-		return end
 
 	#pydub conversion <to_format>
 	raw_audio = AudioSegment.from_file(src, format=from_format)
-	raw_audio.export("\\M1.wav", format=to_format)
+
+	#pydub export to <dst>
+	raw_audio.export(dst, format=to_format)
 
 
+
+src = "transcript.mp3"
+dst = "test.wav"
 start = timer()
-convert("\\M1.mp4")
+convert(src, dst, 'wav')
 end = timer()
 print ("required run time is " + str(end - start))
